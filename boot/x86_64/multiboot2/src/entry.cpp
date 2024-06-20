f2d7f06f81e4fe74538aa02bd9e59279599bee13
cml1010101 <cml1010101@gmail.com>
2024-06-20 17:01:10 -0400

    WIP: Kernel not working yet

--- boot/x86_64/multiboot2/src/entry.cpp
+++ boot/x86_64/multiboot2/src/entry.cpp
@@ -1,5 +1,7 @@
+#include <cstdint>
 extern "C" __attribute__((section(".multiboot.text"))) void _start()
 {
+    *(uint8_t*)0xb8000 = 'H';
     asm volatile("cli");
     asm volatile("hlt");
     while (true)
